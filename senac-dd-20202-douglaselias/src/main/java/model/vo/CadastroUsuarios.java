3d75caf2ebbe65764999ac468b0330c2d7719693
dougl <dougl@Douglas>
2020-08-10 22:03:23 -0300

    primeiro commit

---
+++ senac-dd-20202-douglaselias/src/main/java/model/vo/CadastroUsuarios.java
@@ -0,0 +1,10 @@
+package model.vo;
+
+public class CadastroUsuarios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ senac-dd-20202-douglaselias/src/main/java/view/TelaPrincipal.java
@@ -0,0 +1,68 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+
+public class TelaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipal frame = new TelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bem-vindo a primeira tela!");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.ORANGE);
+		lblNewLabel.setBackground(Color.ORANGE);
+		lblNewLabel.setBounds(48, 11, 318, 14);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Aprovado");
+		btnNewButton.setForeground(Color.ORANGE);
+		btnNewButton.setBackground(Color.DARK_GRAY);
+		btnNewButton.setBounds(73, 71, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Reprovado");
+		btnNewButton_1.setForeground(Color.ORANGE);
+		btnNewButton_1.setBackground(Color.DARK_GRAY);
+		btnNewButton_1.setBounds(204, 71, 89, 23);
+		contentPane.add(btnNewButton_1);
+	}
+}
